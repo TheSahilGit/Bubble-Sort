fec402116d5955a99948bb984228ac9da6b5b10f
Sahil Islam <thesahil.islam@gmail.com>
2020-06-14 19:35:57 +0530

    Array definitions made better.
    Used numpy's features more effectively.

--- BubbleSortDynamicVisual.py
+++ BubbleSortDynamicVisual.py
@@ -1,57 +1,46 @@
-"""Using pygame to see the sorting  dynamically. """
-
-'''This code requires the 'BubbleSort.py' file to be in the same directory '''
-
-### Sahil Islam ###
-### 13/06/2020 ###
-
 from BubbleSort import *
 import numpy as np
 import pygame
 
 pygame.init()
 
-display_width = 900
-display_height = 650
+width = 900
+height = 650
 
 black = [0, 0, 0]
 white = [255, 255, 255]
 red = [200, 0, 0]
 
-display_surface = pygame.display.set_mode((display_width, display_height))
+screen = pygame.display.set_mode((width, height))
 clock = pygame.time.Clock()
 pygame.display.set_caption("Bubble Sort")
 
 
-def rect(x, y, rectHeight, color):
-    rectWidth = 10
+def rect(x, h, color):
+    w = 10
+    pygame.draw.rect(screen, color, [x, height - h, w, h])
 
-    pygame.draw.rect(display_surface, color, [x, y, rectWidth, rectHeight])
 
+w = 10
+n = int(width / w)
+h = np.random.randint(10, height, n)
+x = np.linspace(0, width, n)
 
-display_surface.fill(white)
-n = 100
-x = np.linspace(0, display_width, n, endpoint=False)
-rectHeight = np.zeros(n)
+screen.fill(white)
 
-for i in range(n):
-    rectHeight[i] = np.random.randint(0, display_height)
+for i in range(len(h)):
+    rect(x[i], h[i], black)
 
-for i in range(len(x)):
-    rect(x[i], display_height - rectHeight[i], rectHeight[i], black)
+while True:
+    for i in range(len(h)):
 
-for i in range(len(x)):
-    oneDSort(rectHeight)
+        oneDSort(h)
+        rect(x[i], h[i], red)
 
-    rect(x[i], display_height - rectHeight[i], rectHeight[i], red)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            quit()
-
-    pygame.display.update()
-    clock.tick(30)
-
-pygame.quit()
-quit()
+        pygame.display.update()
+        clock.tick(10)
